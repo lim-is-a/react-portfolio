4274479c608703efe7384ddfa09a34fbecfc315f
iLimas <ilimasc3@gmail.com>
2020-05-04 17:12:25 -0400

    added images directory, and edited Profil  content

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,7 +4,13 @@ export default class Profile extends Component {
     render() {
         return (
             <div>
-                
+                <div>
+                    <img src="public/images/greenSweater.jpg" alt=""/>
+                </div>
+                <div>
+                    <h1>Isabel Limas</h1>
+                    <h3>Full Stack Developer</h3>
+                </div>
             </div>
         )
     }
