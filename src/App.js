746f37892279d66491d18fa39da2a05bef6c42ee
iLimas <ilimasc3@gmail.com>
2020-05-04 19:25:40 -0400

    map through projects, and edited app.js return

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Profile from './components/Profile'
+import ProjectList from './components/ProjectList'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Profile/>
+        <ProjectList/>
       </header>
     </div>
   );
--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,19 +1,29 @@
 import React, { Component } from 'react'
+import ProjectDetail from './ProjectDetail'
 
 export default class ProjectList extends Component {
     render() {
-        const Projects = {
-            title = "Connect Four: Online Game",
-            description = "",
-            image = "",
-            link = ""
-        }
+        const Projects = [
+            {
+                title: "Connect Four: Online Game",
+                description: "bla",
+                image: "blah",
+                link: "blaah"
+            },
+            {
+                title: "Fitness Challenge",
+                description: "bla",
+                image: "blah",
+                link: "blaah"
+            },
+        ]
+
         return (
             <div>
                 <h2>My Projects: </h2>
-                {Projects.map((project, i)=>{
-                    return <Project project={project} key={i}/>
-                })}
+                {Projects.map((project, i) => {
+                    return <ProjectDetail project={project} key={i}/>
+                        })}
             </div>
         )
     }
