0d7c9fb2bca460ff55415da1344b3f7bc672049f
iLimas <ilimasc3@gmail.com>
2020-05-06 15:18:53 -0400

    added list of skills

--- src/components/TechnicalSkills.jsx
+++ src/components/TechnicalSkills.jsx
@@ -4,7 +4,27 @@ export default class TechnicalSkills extends Component {
     render() {
         return (
             <div>
-                <h4>Skills</h4>
+                <h4>Skills: </h4>
+                <div>
+                    <ul>
+                        <li>JavaScript</li>
+                        <li>Python</li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>Node.js</li>
+                        <li>Express</li>
+                        <li>React</li>
+                        <li>Handlebars</li>
+                        <li>Django</li>
+                        <li>PostgreSQL</li>
+                        <li>Git</li>
+                        <li>Github</li>
+                        <li>Heroku</li>
+                        <li>MongoDB</li>
+                        <li>Mongoose</li>
+                        <li>Axios</li>
+                    </ul>
+                </div>
             </div>
         )
     }
