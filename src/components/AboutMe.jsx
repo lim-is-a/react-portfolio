3f538b892718d9993a9f645b60ae477d428e170f
iLimas <ilimasc3@gmail.com>
2020-05-07 19:14:58 -0400

    added flex styling to about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,16 +4,25 @@ import greenSweater from '../images/greenSweater.jpg'
 
 export default class AboutMe extends Component {
     render() {
+        const mainStyle= {
+            display: 'flex',
+        }
+        const style = {
+            display: 'inline-block',
+            margin: '5em',
+        }
         return (
             <div>
-                <section>
-                    <div>
+                <section style={mainStyle}>
+                    <div style={style}>
                         <img src={greenSweater} alt="Isabel in green sweater" />
                     </div>
-                    <h1>About Me: </h1>
-                    <p>
-                        I am a dedicated software engineer that would love to bring my passion for health and biology into the field of technology. Learning and working with new technologies is something I enjoy and would love to work in a collaborative environment with others who share similar interests.
-                    </p>
+                    <div style={style}>
+                        <h1>About Me: </h1>
+                        <p>
+                            I am a dedicated software engineer that would love to bring my passion for health and biology into the field of technology. Learning and working with new technologies is something I enjoy and would love to work in a collaborative environment with others who share similar interests.
+                        </p>
+                    </div>
                 </section>
             </div>
         )
