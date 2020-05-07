0f4aa4f511483e3536425866b6df63a349c7375b
iLimas <ilimasc3@gmail.com>
2020-05-07 19:38:34 -0400

    edited inline style for aboutme

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,20 +4,25 @@ import greenSweater from '../images/greenSweater.jpg'
 
 export default class AboutMe extends Component {
     render() {
-        const mainStyle= {
-            display: 'flex',
-        }
-        const style = {
-            display: 'inline-block',
-            margin: '5em',
+        const styles = {
+            mainStyle: {
+                display: 'flex',
+            },
+            style: {
+                display: 'inline-block',
+                margin: '5em',
+            },
+            center: {
+                paddingTop: '24rem',
+            }
         }
         return (
             <div>
-                <section style={mainStyle}>
-                    <div style={style}>
+                <section style={styles.mainStyle}>
+                    <div style={styles.style}>
                         <img src={greenSweater} alt="Isabel in green sweater" />
                     </div>
-                    <div style={style}>
+                    <div style={styles.style, styles.center}>
                         <h1>About Me: </h1>
                         <p>
                             I am a dedicated software engineer that would love to bring my passion for health and biology into the field of technology. Learning and working with new technologies is something I enjoy and would love to work in a collaborative environment with others who share similar interests.
