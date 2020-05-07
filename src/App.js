c89d57cdaa2df69a3e206bdc96b5f248779ee6a9
iLimas <ilimasc3@gmail.com>
2020-05-07 18:53:09 -0400

    profile header styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Profile from './components/Profile'
+import AboutMe from './components/AboutMe'
 import ProjectList from './components/ProjectList'
 import TechnicalSkills from './components/TechnicalSkills';
 
@@ -9,9 +10,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Profile/>
+      </header>
+        <AboutMe/>
         <ProjectList/>
         <TechnicalSkills/>
-      </header>
     </div>
   );
 }
