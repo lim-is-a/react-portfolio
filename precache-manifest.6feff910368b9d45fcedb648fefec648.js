5dceb2c7334d0b722b1e7acb263a2d7e54f0da0b
iLimas <ilimasc3@gmail.com>
2020-05-08 01:12:11 -0400

    Updates

---
+++ precache-manifest.6feff910368b9d45fcedb648fefec648.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d2eaa5414d801307858ca28b7d0fda2",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "7d0a496507650863a04d",
+    "url": "/react-portfolio/static/css/main.7ed76696.chunk.css"
+  },
+  {
+    "revision": "c0d3e6a057c3dbe839ab",
+    "url": "/react-portfolio/static/js/2.ea232eeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.ea232eeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d0a496507650863a04d",
+    "url": "/react-portfolio/static/js/main.5591d2b3.chunk.js"
+  },
+  {
+    "revision": "37db3f26b7c0ea36aed0",
+    "url": "/react-portfolio/static/js/runtime-main.d08fb9a1.js"
+  },
+  {
+    "revision": "bc34743336912dc3dd6b86b57f0c6e95",
+    "url": "/react-portfolio/static/media/connect4.bc347433.png"
+  },
+  {
+    "revision": "a84a7423a49898c44278ecb65e9883da",
+    "url": "/react-portfolio/static/media/cookbook.a84a7423.png"
+  },
+  {
+    "revision": "b81a9e22ea9793e42f7b9b82a5092946",
+    "url": "/react-portfolio/static/media/earthFriendly.b81a9e22.png"
+  },
+  {
+    "revision": "33c1300ce5cd5deee584bb35368e34d0",
+    "url": "/react-portfolio/static/media/fitChallenge.33c1300c.png"
+  },
+  {
+    "revision": "c0feb2728f559f300ca9d68c405f537d",
+    "url": "/react-portfolio/static/media/greenSweater.c0feb272.jpg"
+  }
+]);
