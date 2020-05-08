7db1210c76327e96a6999fb5c69ed55cd2461e73
iLimas <ilimasc3@gmail.com>
2020-05-08 01:35:32 -0400

    Updates

---
+++ precache-manifest.4032bc59047b1f60e5c47372c3eadf47.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed3f08b8e9f0d43179f717051f221541",
+    "url": "/lim-is-a/react-portfolio/index.html"
+  },
+  {
+    "revision": "e6617b213543ce5b54e0",
+    "url": "/lim-is-a/react-portfolio/static/css/main.7ed76696.chunk.css"
+  },
+  {
+    "revision": "c0d3e6a057c3dbe839ab",
+    "url": "/lim-is-a/react-portfolio/static/js/2.ea232eeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lim-is-a/react-portfolio/static/js/2.ea232eeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6617b213543ce5b54e0",
+    "url": "/lim-is-a/react-portfolio/static/js/main.68ef9b3d.chunk.js"
+  },
+  {
+    "revision": "081ec7056ce90aedcf1a",
+    "url": "/lim-is-a/react-portfolio/static/js/runtime-main.1f0263ae.js"
+  },
+  {
+    "revision": "bc34743336912dc3dd6b86b57f0c6e95",
+    "url": "/lim-is-a/react-portfolio/static/media/connect4.bc347433.png"
+  },
+  {
+    "revision": "a84a7423a49898c44278ecb65e9883da",
+    "url": "/lim-is-a/react-portfolio/static/media/cookbook.a84a7423.png"
+  },
+  {
+    "revision": "b81a9e22ea9793e42f7b9b82a5092946",
+    "url": "/lim-is-a/react-portfolio/static/media/earthFriendly.b81a9e22.png"
+  },
+  {
+    "revision": "33c1300ce5cd5deee584bb35368e34d0",
+    "url": "/lim-is-a/react-portfolio/static/media/fitChallenge.33c1300c.png"
+  },
+  {
+    "revision": "c0feb2728f559f300ca9d68c405f537d",
+    "url": "/lim-is-a/react-portfolio/static/media/greenSweater.c0feb272.jpg"
+  }
+]);
