eb4d590a4ece8dc9dce0dd49b2642447e8388a8f
iLimas <ilimasc3@gmail.com>
2020-05-08 00:58:49 -0400

    added styling to about me section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,29 +4,21 @@ import greenSweater from '../images/greenSweater.jpg'
 
 export default class AboutMe extends Component {
     render() {
-        const styles = {
-            mainStyle: {
-                display: 'flex',
-            },
-            style: {
-                display: 'inline-block',
-                margin: '5em',
-            },
-            center: {
-                paddingTop: '24rem',
-            }
+        const style={
+            display:'flex',
+            margin: '5%',
         }
         return (
-            <div>
-                <section style={styles.mainStyle}>
-                    <div style={styles.style}>
-                        <img src={greenSweater} alt="Isabel in green sweater" />
-                    </div>
-                    <div style={styles.style, styles.center}>
-                        <h1>About Me: </h1>
+            <div className="section me">
+                <h2>About Me: </h2>
+                <section style={style}>
+                    <div  className="about-description">
                         <p>
                             I am a dedicated software engineer that would love to bring my passion for health and biology into the field of technology. Learning and working with new technologies is something I enjoy and would love to work in a collaborative environment with others who share similar interests.
                         </p>
+                    </div>
+                    <div>
+                        <img className="myPic" src={greenSweater} alt="Isabel in green sweater" />
                     </div>
                 </section>
             </div>
