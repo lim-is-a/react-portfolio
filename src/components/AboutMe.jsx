83d5431e0bbf8e13d6b1f210019eb7ff08e9c758
iLimas <ilimasc3@gmail.com>
2020-05-14 15:30:50 -0400

    styling to profile img

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,21 +4,17 @@ import greenSweater from '../images/greenSweater.jpg'
 
 export default class AboutMe extends Component {
     render() {
-        const style={
-            display:'flex',
-            margin: '5%',
-        }
         return (
             <div className="section me">
                 <h2>About Me: </h2>
-                <section style={style}>
+                <section className="about-section">
                     <div  className="about-description">
                         <p>
                             I am a dedicated software engineer that would love to bring my passion for health and biology into the field of technology. Learning and working with new technologies is something I enjoy and would love to work in a collaborative environment with others who share similar interests.
                         </p>
                     </div>
-                    <div>
-                        <img className="myPic" src={greenSweater} alt="Isabel in green sweater" />
+                    <div className="myPic">
+                        <img src={greenSweater} alt="Isabel in green sweater"/>
                     </div>
                 </section>
             </div>
